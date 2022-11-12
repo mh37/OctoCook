1706eabe81601106627241940533a4b0342bbb61
Marc <marc@unit37.org>
2022-11-12 23:24:36 +0200

    add spoonacular detailed recipe and ingredient class

---
+++ OctoCook/Data/API/Spoonacular/SpoonIngredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoCook.Data.API.Spoonacular
+{
+    internal class SpoonIngredient
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float amount { get; set; }
+        public string unit { get; set; }
+    }
+}
---
+++ OctoCook/Data/API/Spoonacular/SpoonRecipeDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoCook.Data.API.Spoonacular
+{
+    internal class SpoonRecipeDetails
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int readyInMinutes { get; set; }
+        public int servings { get; set; }
+        public string sourceUrl { get; set; }
+        public string image { get; set; }
+        public string imageType { get; set; }
+        public string summary { get; set; }
+        public string instructions { get; set; }
+    }
+}
+
