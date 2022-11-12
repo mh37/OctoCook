d48de7a2aaee32a6ecc4229204247da10f3ef765
Marc <marc@unit37.org>
2022-11-12 22:51:56 +0200

    change search results to card layout

--- OctoCook/Data/API/Spoonacular/SpoonRecipe.cs
+++ OctoCook/Data/API/Spoonacular/SpoonRecipe.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +8,9 @@ namespace OctoCook.Data.API.Spoonacular
 {
     public class SpoonRecipe
     {
-        [JsonProperty("id")]
         public int id { get; set; }
-        [JsonProperty("title")]
         public string title { get; set; }
-        [JsonProperty("image")]
         public string image { get; set; }
-        [JsonProperty("imageType")]
         public string imageType { get; set; }
     }
 }
