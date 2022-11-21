51b17eab3a067acdd613c186e959f4e6a3356139
Marc <marc@unit37.org>
2022-11-21 12:59:28 +0200

    add ingredients class

---
+++ OctoCook/Data/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoCook.Data
+{
+    internal class Ingredient
+    {
+        public string name { get; set; }
+        public float amount { get; set; }
+        public string unit { get; set; }
+    }
+}
