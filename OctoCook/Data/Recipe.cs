1c7d5b9c8647bf665ac1ffaa711ce02ac7fc1ff6
Marc <marc@unit37.org>
2022-11-21 12:59:18 +0200

    add more class properties

--- OctoCook/Data/Recipe.cs
+++ OctoCook/Data/Recipe.cs
@@ -1,3 +1,4 @@
+using OctoCook.Data.API.Spoonacular;
 using SQLite;
 namespace EndToEnd.Data;
 public class Recipe
@@ -6,8 +7,10 @@ public class Recipe
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public string Title { get; set; }
+    public int ReadyInMinutes { get; set; }
+    public int Servings { get; set; }
     public string Ingredients { get; set; }
+    public string Summary { get; set; }
     public string Instructions { get; set; }
-    
 
 }
